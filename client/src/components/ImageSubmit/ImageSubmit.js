f3561cf3c2476f884ed0f8b6cdd3204497a0dc73
sarahcheatham <sarah.cheatham14@gmail.com>
2019-05-04 15:29:36 -0500

    updating

--- client/src/components/ImageSubmit/ImageSubmit.js
+++ client/src/components/ImageSubmit/ImageSubmit.js
@@ -15,7 +15,15 @@ class ImageSubmit extends Component{
     }
     render(){
         return(
-            <div className="loginPage">
+            // <FormGroup>
+            //     <Label for="exampleFile">File</Label>
+            //     <Input type="file" name="file" id="exampleFile" />
+            //     <FormText color="muted">
+            //         This is some placeholder block-level help text for the above input.
+            //         It's a bit lighter and easily wraps to a new line.
+            //     </FormText>
+            // </FormGroup>
+            <div className="donation-form">
                 <form action="/api/donations" method="post" encType="multipart/form-data" id="addPhoto"> 
                     <input type="file" name="donation" />
                     <button type="submit" onSubmit={this.handleSubmit}>SAVE</button>
