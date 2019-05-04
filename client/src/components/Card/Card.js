0f46821c9d030817efcd10bce81fd4149f0e3968
Sean Philippi <sean.philippi@gmail.com>
2019-05-04 11:38:47 -0500

    initial card commit

---
+++ client/src/components/Card/Card.js
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react';
+// import './Card.css';
+
+class Card extends PureComponent {
+
+  render() {
+
+    return (
+      <div>
+        Card component
+      </div>
+    );
+  }
+}
+
+// Card.propTypes = {
+// }
+
+export default Card;
